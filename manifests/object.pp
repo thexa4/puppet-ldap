9b910c2dad54eb7527015bf4a6add258986c11d7
Max Maton <max@maton.info>
2016-07-31 20:38:57 +0000

    Initial commit

---
+++ manifests/object.pp
@@ -0,0 +1,38 @@
+define ldap::object (
+	$attributes,
+	$ensure,
+	$adduser,
+	$addpw,
+){
+	if $ensure == "present" {
+		exec { "ldapadd $title":
+			command => "/usr/bin/ldapadd -xy '/etc/ldap/additions/$title.pwdfile' -D '$adduser' -f '/etc/ldap/additions/$title'",
+			unless => "/usr/bin/ldapsearch -xy '/etc/ldap/additions/$title.pwdfile' -D '$adduser' -b '$title' -s base",
+			require => [ File["/etc/ldap/additions/$title"], File["/etc/ldap/additions/$title.pwdfile"]],
+		}
+
+		if ! defined(File["/etc/ldap/additions"]) {
+			file { "/etc/ldap/additions":
+				ensure => directory,
+				mode => '770',
+			}
+		}
+
+		file { "/etc/ldap/additions/$title":
+			ensure => file,
+			content => template("ldap/ldapadd.erb"),
+			mode => '660',
+			require => File["/etc/ldap/additions"],
+		}
+		
+		file { "/etc/ldap/additions/$title.pwdfile":
+			ensure => file,
+			content => $addpw,
+			mode => '660',
+			require => File["/etc/ldap/additions"],
+		}
+
+	} elsif $ensure == "absent" {
+
+	} 
+}
