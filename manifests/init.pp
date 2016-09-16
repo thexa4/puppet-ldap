72f27919d52af21bd5bbc3a3f3fc093a6e724be0
Max Maton <max@maton.info>
2016-09-16 09:37:17 +0000

    First implementation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,19 @@
 class ldap(
-		$base,
-		$uri,
-		$ssl = false,
-	) {
-	
-	if ($ssl) {
-		class { "openldap::client":
-			base => $base,
-			uri => $uri,
-			tls_cacert => "/etc/ssl/certs/host-ca.crt",
-		}
-	} else {
-		class { "openldap::client":
-			base => $base,
-			uri => $uri,
-		}
-	}
+    $base,
+    $uri,
+    $ssl = false,
+  ) {
+  
+  if ($ssl) {
+    class { 'openldap::client':
+      base       => $base,
+      uri        => $uri,
+      tls_cacert => '/etc/ssl/certs/host-ca.crt',
+    }
+  } else {
+    class { 'openldap::client':
+      base => $base,
+      uri  => $uri,
+    }
+  }
 }
